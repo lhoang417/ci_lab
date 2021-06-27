2b60311961536b2ee1762df09581c30eb85ffbf1
Linh Hoang <Linh.Hoang@devmounta.in>
2021-06-27 16:02:39 -0500

    second

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('can create a basic greeting', () => {
+  expect(getGreeting('Linh')).toBe("Hi Linh");
+});
